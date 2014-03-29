6bbae4ea737c496016a8147a4a8c99c1d65d363d
Arup Rakshit <aruprakshit@rocketmail.com>
2014-03-29 15:39:58 +0530

    refactored the method final_report_data_as_a_hash, to reduce the responsiblity of this method.

--- file_report.rb
+++ file_report.rb
@@ -10,6 +10,7 @@ module FileReport
   # setting the default value, to take the gravity as 1 for file types that are not listed
   # @gravities_of_files.
   @@gravities_of_files.default = 1 
+
   def parse_json(json)
     require 'json'
     JSON.parse(json)["files"].map { |h| h.values_at('size', 'name', 'extension') }
@@ -20,9 +21,13 @@ module FileReport
       resource_lists_to_access(1)
       files_after_classification = file_classification parse_json(@josn_files ) 
       final_report_data_as_a_hash = final_report_data_as_a_hash(files_after_classification)
-      total_gravity = total_gravity_calculation(final_report_data_as_a_hash)
-      added_gravity = added_gravity_calculation(final_report_data_as_a_hash)
-      [total_gravity,added_gravity,final_report_data_as_a_hash]
+      #total_gravity = total_gravity_calculation(final_report_data_as_a_hash)
+      #added_gravity = added_gravity_calculation(final_report_data_as_a_hash)
+      [ 
+        total_gravity_calculation(final_report_data_as_a_hash),
+        added_gravity_calculation(final_report_data_as_a_hash),
+        final_report_data_as_a_hash
+      ]
     rescue RuntimeError => ex
       case ex.message
       when 'Invalid resource access' then []
@@ -33,7 +38,7 @@ module FileReport
 
   def file_classification(parsed_json)
     parsed_json.map do |a|
-      a.fill(@@category_of_files.find { |k,v| v.include?(a.last) }.first,-1,1)
+      a[-1] = @@category_of_files.find { |k,v| v.include?(a.last) }.first ; a
     end
   end
 
@@ -48,9 +53,17 @@ module FileReport
   end
 
   def final_report_data_as_a_hash(classified_documents)
-    Hash[classified_documents.group_by { |a| a.last }.map do |documen_type,document_information|
-      gravity_of_document = (document_information.map(&:first).inject(:+) * @@gravities_of_files[documen_type])/1000
-      [documen_type, [document_information.size,gravity_of_document.round(2)]]
+    Hash[classified_documents.group_by { |a| a.last }.map do |document_type,document_information|
+     [
+        document_type, 
+        [  
+          document_information.size, calculate_gravity_of_each_document(document_information,document_type)
+        ]
+      ]
     end]
+  end
+
+  def calculate_gravity_of_each_document(document_information,document_type)
+    ((document_information.map(&:first).inject(:+) * @@gravities_of_files[document_type])/1000).round(2)
   end
 end
