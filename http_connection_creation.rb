0298490782ff5c421a8837b0422e03f14a611e06
Arup Rakshit <aruprakshit@rocketmail.com>
2014-03-29 21:53:52 +0530

    existing method 'create_persistent_connections' has been refactored in file http_connection_creation.rb.

--- http_connection_creation.rb
+++ http_connection_creation.rb
@@ -1,11 +1,11 @@
 require "net/http"
 
 module HttpConnection
-  def login_to_host(resources)
-      create_persistent_connections(resources)
+  def login_to_host(resources = nil)
+    create_persistent_connections(resources)
   end
 
-  def create_persistent_connections(resources)
+  def create_persistent_connections(resources = nil)
     uri = URI(@base_url)
     # creating the secure HTTP connection.
     Net::HTTP.start(uri.host, uri.port,:use_ssl => true) do |http|
@@ -16,17 +16,19 @@ module HttpConnection
       else
         raise "Invalid credentials"
       end
-      # collecting all the cookies provided by host to access the other resources from the target host
-      cookies = response.get_fields('set-cookie').map do |cookie|
-        cookie.split('; ')[0]
-      end
-      cookie_hash = { 'Cookie' => cookies.join('; ') }
-      resources.each { |resource| send(resource,http,cookie_hash) }
+      resources.each { |resource| send(resource,http,collect_cookies(response)) }
     end
   end
 
   def make_url(apipath)
     @base_url + "/api/open-v1.0/" + apipath
+  end
+
+  def collect_cookies(response)
+    # collecting only the cookies provided by host to access the other resources from the target host
+    cookies = response.get_fields('set-cookie').map { |cookie| cookie.split('; ')[0] }
+    #combining the cookies and returned
+    cookie_hash = { 'Cookie' => cookies.join('; ') }
   end
 
   private :make_url, :login_to_host, :create_persistent_connections
