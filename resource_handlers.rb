1239a53e4953a2c497fad19bde70ed53e41388c7
Arup Rakshit <aruprakshit@rocketmail.com>
2014-03-30 12:44:52 +0530

    added some descriptions to each method resides in resource_handlers.rb file

--- resource_handlers.rb
+++ resource_handlers.rb
@@ -8,13 +8,17 @@ module ResourceHandler
     # target servers
     response = http_connection.get URI(make_url('files.json')).path, cookie_hash
     if response.code == '200'
-      puts "able to extract the list of files successfully"
+      # able to extract the list of files successfully.
       @josn_files = response.body # gives the file list as a JSON hash
     else
+      # if the cookie expired or or http connection is lost or API url to access the file is changed, then error will
+      # be thrown.
       puts "couldn't fetch the file lists from host, so try again"
     end
   end
 
+  # this method knows, what's the id of each specific resource handlers and will log in to the host and then call the
+  # specific handler.
   def resource_lists_to_access(action_handler)
     resource_hash = { 1 => :get_files }
     # trying to log in using the users specified credentials. If log in is not possible, a message will be displayed
